64963e737d62c6744b86fcfb677b393cbdd501d1
Parshkhadka07 <parashkhadka020@gmail.com>
2026-07-28 22:59:26 +0545

    model added

--- BACKEND/monitor/admin.py
+++ BACKEND/monitor/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Device
 
-# Register your models here.
+@admin.register(Device)
+class DeviceAdmin(admin.ModelAdmin):
+    list_display = ('device_name', 'ip_address', 'owner', 'created_at')
+    list_filter = ('owner',)
+    search_fields = ('device_name', 'ip_address')
--- BACKEND/monitor/models.py
+++ BACKEND/monitor/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Device(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    device_name = models.CharField(max_length=100)
+    ip_address = models.GenericIPAddressField()
+    location = models.CharField(max_length=100, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.device_name
